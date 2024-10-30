f96722f5f385d4346210a6f78544e65d7c45349d
phj <phjlia2430@naver.com>
2024-10-31 04:09:26 +0900

    [Refactor] 코드 리팩토링 실습과제2 useForm Hook

--- movie-site/src/page/Loginpage.jsx
+++ movie-site/src/page/Loginpage.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import {useForm} from 'react-hook-form'
-import * as yup from 'yup'
-import {yupResolver} from '@hookform/resolvers/yup'
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import useForm from '../hooks/use-form';
+import { validationLogin } from '../utils/validate';
 
-const Container = styled.div`
+/*const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -12,6 +13,7 @@ const Container = styled.div`
   height: 50vh;
   color: white;
 `;
+
 const Form = styled.form`
   display: flex;
   flex-direction: column;  
@@ -21,20 +23,16 @@ const Form = styled.form`
 const Input = styled.input`
   padding: 10px;  
   border-radius: 10px;
-  width:400px;  
-  background-color:white;
-
-  &:hover {
-  
-  }
+  width: 400px;  
+  background-color: white;
 `;
 
 const LoginButton = styled.input`
   padding: 10px;  
   border-radius: 10px;
-  width:420px;
+  width: 420px;
   background-color: ${props => (props.disabled ? 'gray' : '#c4006a')};
-  color:white;
+  color: white;
 
   &:hover {
     background-color: ${props => (props.disabled ? 'gray' : '#000080')};
@@ -45,36 +43,138 @@ const LoginButton = styled.input`
 const Loginpage = () => {
   const schema = yup.object().shape({
     email: yup.string().email('올바른 이메일 형식이 아닙니다. 다시 확인해주세요!').required('이메일은 필수 입력 사항입니다.'),
-    password: yup.string().min(8,'비밀번호는 8-16자 사이로 입력해주세요!').max(16,'비밀번호는 8-16자 사이로 입력해주세요!').required('비밀번호는 8-16자 사이로 입력해주세요!'),
-})
+    password: yup.string().min(8, '비밀번호는 8-16자 사이로 입력해주세요!').max(16, '비밀번호는 8-16자 사이로 입력해주세요!').required('비밀번호는 필수 입력 사항입니다.'),
+  });
 
-const {register, handleSubmit, formState: {errors}} = useForm({
+  const { register, handleSubmit, formState: { errors }, setValue } = useForm({
     resolver: yupResolver(schema),
     mode: 'onChange',
-});
+  });
 
-const onSubmit = (data) => {
-    console.log('폼 데이터 제출')
+  const onSubmit = (data) => {
+    console.log('폼 데이터 제출');
     console.log(data);
-}
+  };
 
-const isDisabled = Object.keys(errors).length > 0;
+  const isDisabled = Object.keys(errors).length > 0;
 
-return (
-  <Container>
-    <h3>로그인</h3> {}
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <Input type={'email'} {...register("email")} placeholder="이메일을 입력해주세요!" />
-      <p style={{ color: 'red' }}>{errors.email?.message}</p>
-      <Input type={'password'} {...register("password")} placeholder="비밀번호를 입력해주세요!" />
-      <p style={{ color: 'red' }}>{errors.password?.message}</p>
-      <LoginButton type={'submit'} value="로그인"  disabled={isDisabled}/>
+  return (
+    <Container>
+      <h3>로그인</h3>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Input 
+          type='email' 
+          placeholder="이메일을 입력해주세요!" 
+          onChange={(e) => setValue('email', e.target.value)} 
+          {...register("email")} 
+        />
+        <p style={{ color: 'red' }}>{errors.email?.message}</p>
+        <Input 
+          type='password' 
+          placeholder="비밀번호를 입력해주세요!" 
+          onChange={(e) => setValue('password', e.target.value)} 
+          {...register("password")} 
+        />
+        <p style={{ color: 'red' }}>{errors.password?.message}</p>
+        <LoginButton type='submit' value="로그인" disabled={isDisabled} />
+      </Form>
+    </Container>
+  );
+};
+
+export default Loginpage;*/
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 50vh;
+  color: white;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;  
+  align-items: center;  
+`;
+
+const Input = styled.input`
+  padding: 10px;  
+  margin:10px 0;
+  border-radius: 10px;
+  width: 400px;  
+  background-color: white;
+  border: ${props => props.error ? '4px solid red' : '1px solid #ccc'};
+
+  &:focus {
+    border-color: #000080;
+  }
+`;
+
+const LoginButton = styled.button` 
+  padding: 10px;  
+   margin:10px 0;
+  border-radius: 10px;
+  width: 420px; 
+  background-color: ${props => (props.disabled ? 'gray' : '#c4006a')};
+  color: white;
+  border: none;
+
+  &:hover {
+    background-color: ${props => (props.disabled ? 'gray' : '#000080')};
+    color: #fff;
+  }
+`;
+
+const ErrorText = styled.h1`
+  color: red;
+  font-size: 12px;
+`;
+
+const Loginpage = () => {
+
+  const login = useForm({
+    initialValue: {
+      email: '',
+      password: '',
+    },
+    validate: validationLogin,
+  });
+
+  const handlePressLogin=(e)=>{
+    e.preventDefault();
+    console.log(login.values.email, login.values.password)
+  }
+  
+  return (
+    <Container>
+    <h3>로그인</h3>
+    <Form onSubmit={handlePressLogin}>
+    {/* 
+    <input
+      onBlur={() => handleBlur('email')}
+      value={values.email}
+      onChange={(e) => handleChangeInput('email', e.target.value)}
+    />
+    <input
+      onBlur={() => handleBlur('password')}
+      value={values.password}
+      onChange={(e) => handleChangeInput('password', e.target.value)}
+    />
+    */}
+
+    <Input error={login.touched.email && login.errors.email} type={'email'} 
+    placeholder='이메일을 입력해주세요!' {...login.getTextInputProps('email')}/>
+    {login.touched.email && login.errors.email && <ErrorText>{login.errors.email}</ErrorText>}
+    <Input error={login.touched.password && login.errors.password} type={'password'} 
+    placeholder='비밀번호를 입력해주세요!' {...login.getTextInputProps('password')}/>
+    {login.touched.password && login.errors.password && <ErrorText>{login.errors.password}</ErrorText>}
+
+    <LoginButton type="submit">로그인</LoginButton>
     </Form>
   </Container>
-);
+  );
 };
 
 export default Loginpage;
-
-
-
---
+++ movie-site/src/utils/validate.js
@@ -0,0 +1,28 @@
+const emailPattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+$/; 
+
+function validateUser(values) { 
+    const errors = {
+        email: '',
+        password: '',
+    };
+
+    if (!values.email) {
+        errors.email = '이메일은 필수 입력 사항입니다.';
+    } else if (!emailPattern.test(values.email)) {
+        errors.email = '올바른 이메일 형식이 아닙니다. 다시 확인해주세요!';
+    }
+
+    if (!values.password) {
+        errors.password = '비밀번호는 필수 입력 사항입니다.'; 
+    } else if (values.password.length < 8 || values.password.length > 16) {
+        errors.password = '비밀번호는 8~16자 사이로 입력해주세요!';
+    }
+
+    return errors;
+}
+
+function validationLogin(values) {
+    return validateUser(values);
+}
+
+export { validationLogin };
